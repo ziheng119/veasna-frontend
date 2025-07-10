6eb652b154705f8a10011c85fb43fd728c9f293c
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-07-10 13:03:59 +0800

    feat: Adjusted Individual Patient List Add/Edit Tabs, Added patientTabs

--- src/components/patient-form/patientTabs.tsx
+++ src/components/patient-form/patientTabs.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, {useState} from 'react';
+import { PatientListTab, PATIENTINFO_TAB, VITALS_TAB, HEF_TAB } from '@/models/patient-list/patientlist_tab';
 import Tab from '@/components/shared/Tab';
-import PatientInfo from './patientInfo';
+import PatientInfo from './PatientInfo';
 import Vitals from './Vitals';
 import HEF from './HEF';
+import { useState, useEffect } from 'react';
 
 interface PatientData {
     // Patient Info
@@ -20,7 +21,7 @@ interface PatientData {
     weight: string;
     bmi: string;
     category: string;
-    isBelow3rdpercentile: string;
+    isBelow3rdPercentile: boolean;
     bloodPressure: string;
     temperature: string;
     additionalNotes: string;
@@ -38,7 +39,48 @@ interface Props {
 }
 
 export default function PatientTabs({ patient, onUpdatePatient, onSave, onCancel }: Props) {
-    const [activeTab, setActiveTab] = useState<PatientTabType>(PATIENT_INFO);
+    const [activeTab, setActiveTab] = useState<PatientListTab>(PATIENTINFO_TAB);
 
-    const apply
+    const applyTab = (activeTab: PatientListTab) => {
+        switch (activeTab) {
+            case PATIENTINFO_TAB:
+                return <PatientInfo patient={patient} onUpdatePatient={onUpdatePatient}/>
+            case VITALS_TAB:
+                return <Vitals patient={patient} onUpdatePatient={onUpdatePatient} />
+            case HEF_TAB:
+                return <HEF patient={patient} onUpdatePatient={onUpdatePatient} />
+            default:
+                return <PatientInfo patient={patient} onUpdatePatient={onUpdatePatient} />
+        }
+    }
+
+    useEffect(() => {
+        applyTab(activeTab)
+    }, [activeTab])
+
+    return (
+        <div>
+            <div className='flex'>
+                <Tab
+                    label="Patient Info"
+                    isActive={activeTab === PATIENTINFO_TAB}
+                    onClick={() => setActiveTab(PATIENTINFO_TAB)}
+                />
+                <Tab
+                    label="Vitals"
+                    isActive={activeTab === VITALS_TAB}
+                    onClick={() => setActiveTab(VITALS_TAB)}
+                />
+                <Tab
+                    label="HEF"
+                    isActive={activeTab === HEF_TAB}
+                    onClick={() => setActiveTab(HEF_TAB)}
+                />
+            </div>
+
+            <div className='bg-beige-default p-7'>
+                {applyTab(activeTab)}
+            </div>
+        </div>
+    )
 }
--- src/models/patient-list/patientlist_tab.ts
+++ src/models/patient-list/patientlist_tab.ts
@@ -1,1 +1,5 @@
-export type PatientListTab = ''
+export type PatientListTab = 'patient info' | 'vitals' | 'HEF';
+
+export const PATIENTINFO_TAB = 'patient info';
+export const VITALS_TAB = 'vitals';
+export const HEF_TAB = 'HEF';
