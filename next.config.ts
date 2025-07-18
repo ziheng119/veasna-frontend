9d0e627d96fafecff1f841fc884a5bd0a9aff49d
jowhee3011 <joeygoh3011@gmail.com>
2025-07-18 15:05:55 +0800

    add page bare bones

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // ✅ NEW static export method
+  images: {
+    unoptimized: true, // ✅ disables built-in optimization
+  },
   // ...other options
 };
 
---
+++ src/components/physiotherapy/PhysiotherapyNotesContainer.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import VerticalLabelInputPair from '../shared/VerticalLabelInputPair'
+import SaveButton from '../shared/SaveButton'
+import Image from 'next/image'
+
+export default function PhysiotherapyNotesContainer() {
+
+  return (
+    <div className="flex flex-col gap-4 bg-beige-default px-4 py-2 rounded-md border-[1px] lg:w-[30%]">
+      <h2 className="text-[20px] font-semibold">Consultation Notes</h2>
+      <div className="flex w-full h-[40%]">
+        <VerticalLabelInputPair />
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <h2 className="text-[20px] font-semibold">Body Chart Indication</h2>
+        <div className="flex flex-row w-full gap-2">
+          <Image
+            src="/bodychart_front.jpg"
+            alt="Body Chart Front"
+            className="w-1/2 h-auto object-contain"
+            width={300}
+            height={500}
+          />
+          <Image
+            src="/bodychart_back.jpg"
+            alt="Body Chart Back"
+            width={300}
+            height={500}
+            className="w-1/2 h-auto object-contain"
+          />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-end">
+        <SaveButton />
+      </div>
+    </div>
+  )
+}
