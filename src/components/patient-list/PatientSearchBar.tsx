c637339bad1a0bfbdd3c0d8dcb0a6f36ff1774a8
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-07-19 10:28:06 +0800

    fix: align SearchIcon usage to new component

--- src/components/patient-list/PatientSearchBar.tsx
+++ src/components/patient-list/PatientSearchBar.tsx
@@ -10,13 +10,16 @@ export const PatientSearch: React.FC<PatientSearchProps> = ({ onSearch, onAddPat
     return (
         <div className='flex items-center gap-4 p-4'>
             <div className='relative flex-1'>
-                <SearchIcon className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4' />
-                <input
-                    type='text'
-                    placeholder='Search Patient'
-                    className='text-black w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500'
-                    onChange={(e) => onSearch ?.(e.target.value)}
-                />
+                <div className='absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400'>
+                    <SearchIcon />
+                </div>                
+                
+                    <input
+                        type='text'
+                        placeholder='Search Patient'
+                        className='text-black w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500'
+                        onChange={(e) => onSearch ?.(e.target.value)}
+                    />
             </div>
             <button
                 onClick={onAddPatient}
